dbece916ab3666a3b26548a9a742d74af24093c3
Mohammed Yaseen <mohammed.yaseen@yellow.ai>
2026-09-21 14:21:20 +0530

    feat: persistent logging

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,14 @@ use tower_http::trace::TraceLayer;
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
-    tracing_subscriber::fmt::init();
+    let file_appender = tracing_appender::rolling::daily("logs", "crud-api.log");
+    let (non_blocking_writer, _guard) = tracing_appender::non_blocking(file_appender);
+
+    tracing_subscriber::fmt()
+        .with_writer(non_blocking_writer)
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_ansi(false)
+        .init();
 
     let pool = db::connect_db().await.expect("Failed to connect to DB.");
     let jwt = std::env::var("JWT_SECRET").expect("JWT_SECRET should be set.");
